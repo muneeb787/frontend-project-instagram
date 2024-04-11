adce18c1a939b25edeedcbc5cb4d2e320f9953a9
Muneeb <muneebahmad1268@gmail.com>
2024-04-11 16:16:47 +0500

    setting routes issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -26,5 +30,4 @@ export default {
     },
   },
   plugins: [],
-}
-
+};
